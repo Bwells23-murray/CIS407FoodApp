918caff2648e0c8e3e636be70cdfdf636f9b4b47
tpalmer8 <tpalmer8@murraystate.edu>
2025-11-10 15:52:02 -0600

    Commiting to main

--- CIS407 Project/McDougals.py
+++ CIS407 Project/McDougals.py
@@ -7,22 +7,23 @@ root = tk.Tk()
 root.title ("Welcome to McDougals")
 root.geometry("300x550")
 
-img = PhotoImage(file='Assets/McDonalds arch.png')
+img = PhotoImage(file='CIS407 Project/Assets/McDonalds arch.png')
 image_label = tk.Label(root, image=img)
-image_label.pack
+image_label.pack(
+)
 
 
 loginButton = tk.Button(root, text="Login")
 registerButton = tk.Button(root, text="Register")
 
 loginButton.pack(
-    ipadx=10,
+    ipadx=5,
     ipady=5,
     expand=True
 )
 
 registerButton.pack(
-    ipadx=10,
+    ipadx=5,
     ipady=5
 )
 
