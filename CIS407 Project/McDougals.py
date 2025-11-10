cc575b2394f8ea0c50f580facbb3bbc4b8a94c66
Reese Palmer <157412467+tpalmer8@users.noreply.github.com>
2025-11-10 15:26:05 -0600

    Add files via upload
    Adding the files from VS Code.

---
+++ CIS407 Project/McDougals.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+from tkinter.ttk import *
+from tkinter import PhotoImage
+
+#main application window
+root = tk.Tk()
+root.title ("Welcome to McDougals")
+root.geometry("300x550")
+
+img = PhotoImage(file='Assets/McDonalds arch.png')
+image_label = tk.Label(root, image=img)
+image_label.pack
+
+
+loginButton = tk.Button(root, text="Login")
+registerButton = tk.Button(root, text="Register")
+
+loginButton.pack(
+    ipadx=10,
+    ipady=5,
+    expand=True
+)
+
+registerButton.pack(
+    ipadx=10,
+    ipady=5
+)
+
+root.mainloop()
